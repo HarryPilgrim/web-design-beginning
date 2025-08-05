18c542a41e93e781aae45ebc846c3f09d051ea48
Harry <76102114+HarryPilgrim@users.noreply.github.com>
2025-08-05 19:49:08 +0100

    update contact info

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,8 +1,8 @@
 module.exports = {
     name: "Neon Flow Web Designs",
-    email: "help@codestitch.app",
-    phoneForTel: "555-779-4407",
-    phoneFormatted: "(555) 779-4407",
+    email: "hello@neonflow.org",
+    phoneForTel: "+447754970564",
+    phoneFormatted: "+44 7754 970 564",
     address: {
         lineOne: "Neon Flow",
         lineTwo: "Headquarters",
@@ -17,7 +17,7 @@ module.exports = {
         instagram: "https://www.instagram.com/",
     },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    domain: "https://www.example.com",
+    domain: "https://www.neonflow.org",
     // Passing the isProduction variable for use in HTML templates
     isProduction: process.env.ELEVENTY_ENV === "PROD",
 };
